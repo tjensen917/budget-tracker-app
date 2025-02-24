fcb21be6c1d39afbdcd2b055e1d783477b77ba79
tjensen917 <tjensen917@outlook.com>
2025-02-24 14:35:35 -0600

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://budget-tracker-app-backend-41ib.onrender.com";
 
 export const addExpense = (expense, userId) => 
   axios.post(`${API_URL}/add-expense`, { ...expense, user_id: userId });
