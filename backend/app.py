066580f27765a6138937e6e3c60072d1deafb613
tjensen917 <tjensen917@outlook.com>
2025-02-24 17:16:48 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,13 @@ from datetime import datetime
 app = Flask(__name__)
 CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///budget_tracker.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Import models after db is defined
+import models
 
 # Expense Model
 class Expense(db.Model):
