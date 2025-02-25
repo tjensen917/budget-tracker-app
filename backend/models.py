b3e91d91d451a1f7387e1e6c36aa0a88abd12206
tjensen917 <tjensen917@outlook.com>
2025-02-24 18:57:28 -0600

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,12 @@
 from app import db
+from flask_sqlalchemy import SQLAlchemy
 
+db=SQLAlchemy()
 class Expense(db.Model):
+    __tablename__ = 'expense'
+    __table_args__ = {'extend_existing': True}  # This allows existing tables to be reused
+
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Float, nullable=False)
